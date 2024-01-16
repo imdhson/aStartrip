9a7d699b2636abb69ac5b08e7bcdd7ff5f22e7b3
imdhson <hhammer1234@gmail.com>
2024-01-16 13:54:54 +0900

    init

---
+++ src/main/java/ce/daegu/ac/kr/aStartrip/AStartripApplication.java
@@ -0,0 +1,13 @@
+package ce.daegu.ac.kr.aStartrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AStartripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AStartripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ce/daegu/ac/kr/aStartrip/AStartripApplicationTests.java
@@ -0,0 +1,13 @@
+package ce.daegu.ac.kr.aStartrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AStartripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
