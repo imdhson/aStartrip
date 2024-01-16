f8120a1c1a63da9576295c17b97d5a2e9cb16b31
imdhson <hhammer1234@gmail.com>
2024-01-16 16:19:09 +0900

    add main.css header

--- src/main/java/ce/daegu/ac/kr/aStartrip/controller/IndexController.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/controller/IndexController.java
@@ -2,13 +2,15 @@ package ce.daegu.ac.kr.aStartrip.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name", "test");
         return "index";
     }
 }
