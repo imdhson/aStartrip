8ec3c4e6c90859c7156c54bfda02220fbacc54b3
LeeBeakSeung <dodomom771@gmail.com>
2024-01-18 10:21:39 +0900

    회원가입

--- src/main/java/ce/daegu/ac/kr/aStartrip/controller/RegistController.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/controller/RegistController.java
@@ -1,15 +1,30 @@
 package ce.daegu.ac.kr.aStartrip.controller;
 
+import ce.daegu.ac.kr.aStartrip.dto.MemberDTO;
+import ce.daegu.ac.kr.aStartrip.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
 public class RegistController {
+
+    @Autowired
+    private MemberService memberService;
+
     @GetMapping("/regist")
     public String regist() {
         log.info("regist form");
         return "regist";
+    }
+
+    @PostMapping("/regist")
+    public String doRegist(MemberDTO dto, Model model){
+        log.info(dto.toString());
+        return "";
     }
 }
