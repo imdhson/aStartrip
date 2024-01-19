7aba9460009f0032c1a20bd7201f4f273a401d96
imdhson <hhammer1234@gmail.com>
2024-01-19 10:42:18 +0900

    added reading writing

--- src/main/java/ce/daegu/ac/kr/aStartrip/entity/Article.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/entity/Article.java
@@ -1,6 +1,8 @@
 package ce.daegu.ac.kr.aStartrip.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -14,6 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 public class Article extends BaseEntity{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long num;
     private String title;
     private String writer;
