73dffa326917e93f3962cee8cf37e2a43a9eeebe
imdhson <hhammer1234@gmail.com>
2024-01-19 17:23:22 +0900

    remove login,registController

--- src/main/java/ce/daegu/ac/kr/aStartrip/controller/UserController.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/controller/UserController.java
@@ -2,6 +2,7 @@ package ce.daegu.ac.kr.aStartrip.controller;
 
 import ce.daegu.ac.kr.aStartrip.dto.MemberDTO;
 import ce.daegu.ac.kr.aStartrip.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,10 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
-public class LoginController {
-
-    @Autowired
-    private MemberService memberService;
+@RequiredArgsConstructor
+public class UserController {
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public String loginID() {
@@ -30,6 +30,23 @@ public class LoginController {
         if (memberService.findID(dto.getEmail())) {
             return "login";
         } else {
+            return "regist";
+        }
+    }
+
+    @GetMapping("/regist")
+    public String regist() {
+        log.info("regist form");
+        return "regist";
+    }
+
+    @PostMapping("/regist")
+    public String doRegist(MemberDTO dto, Model model) {
+        log.info(dto.toString());
+        if (memberService.register(dto)) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("email", dto.getEmail());
             return "regist";
         }
     }
