a77bb74adce111bd0998e5df2dead6ec2c6293c6
LeeBeakSeung <dodomom771@gmail.com>
2024-01-24 15:23:33 +0900

    merge

--- src/main/java/ce/daegu/ac/kr/aStartrip/service/ArticleServiceImpl.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/service/ArticleServiceImpl.java
@@ -45,7 +45,15 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public void changeArticle(Member member, ArticleDTO articleDTO) {
+        if(member.getName().equals(articleDTO.getWriter())) {
+            Optional<Article> entity = articleRepository.findById(articleDTO.getNum());
+            if(entity.isPresent()) {
+                Article article = entity.get();
+                article = updateArticle(article, articleDTO);
 
+                articleRepository.save(article);
+            }
+        }
     }
 
 }
