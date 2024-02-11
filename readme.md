7e21ad690c9154232bfd19f387f36a792ad2dad2
imdhson <hhammer1234@gmail.com>
2024-02-11 16:33:42 +0900

    bard api 충돌 해결 임시로 pip uninstall bardapi 해야함

--- readme.md
+++ readme.md
@@ -3,5 +3,8 @@ pip install sqlalchemy<br>
 pip install mysqlclient<br>
 pip install pymysql<br>
 pip install python-dotenv<br>
-
+three js <br>
+qrcode js<br>
+<br>
+<br>
 src/main/.env에서 주소 변경해야함.
--- src/main/bardtest.py
+++ src/main/bardtest.py
@@ -1,4 +1,8 @@
 import sys, os
+
+#bard api -> gemini 호환 버전으로 수정
+sys.path.append("/bardapi")
+
 import time
 from dotenv import load_dotenv
 from bardapi import Bard
@@ -7,13 +11,16 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 
+
 load_dotenv()
 
 cookie_dict = {
     "__Secure-1PSID": os.getenv("BARD__Secure-1PSID"),
     "__Secure-1PSIDTS": os.getenv("BARD__Secure-1PSIDTS"),
     "__Secure-1PSIDCC": os.getenv("BARD__Secure-1PSIDCC"),
+    "NID": os.getenv("BARD__NID"),
+    "GOOGLE_ABUSE_EXEMPTION":os.getenv("BARD__GOOGLE_ABUSE_EXEMPTION")
 }
 
 bard = BardCookies(cookie_dict=cookie_dict)
-print(bard.get_answer("こんにちは")['content']) ##111111
+print(bard.get_answer("hello. what is your name?")['content']) ##111111
