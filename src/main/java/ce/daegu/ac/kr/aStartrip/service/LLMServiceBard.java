ffe418c451f9f740d8e901cb35fd0819ad9bdc10
imdhson <mail@imdhson.com>
2024-10-12 22:07:43 +0900

    replace the bard api to PPLX api

--- src/main/java/ce/daegu/ac/kr/aStartrip/service/LLMServiceBard.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/service/LLMServiceBard.java
@@ -1,92 +1,92 @@
-package ce.daegu.ac.kr.aStartrip.service;
-
-import ce.daegu.ac.kr.aStartrip.broadcast.BroadcastService;
-import ce.daegu.ac.kr.aStartrip.dto.CardDTO;
-import ce.daegu.ac.kr.aStartrip.entity.Card;
-import ce.daegu.ac.kr.aStartrip.entity.LLMStatusENUM;
-import ce.daegu.ac.kr.aStartrip.repository.CardRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Optional;
-
-@RequiredArgsConstructor
-@Service
-@Slf4j
-@Component
-public class LLMServiceBard implements LLMService {
-    private final CardRepository cardRepository;
-    private final BroadcastService broadcastService;
-
-    @Value("${python.path}")
-    private String pythonPath;
-
-    public static boolean running = false;
-
-    @Override
-    public boolean execute(Card entity) {
-        while (running) {
-            try {
-                Thread.sleep(6000); //6초 대기
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        try {
-            Thread.sleep(2000); //2초 대기 어뷰징 방지용
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        log.debug("현재 디렉터리:{}", System.getProperty("user.dir"));
-
-        try {
-            running = true;
-            Process process = new ProcessBuilder
-                    (pythonPath, "./src/main/bard.py",
-                            String.valueOf(entity.getId())).start();
-            // $ python ./src/main/bard.py 카드ID
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                log.debug("Bard python: {}", line);
-                if (line.trim().equals("true")) {
-                    log.debug("Bard 성공");
-                    running = false;
-                    return true;
-                }
-            }
-        } catch (IOException e) {
-            entity.setLlmStatus(LLMStatusENUM.CANCELED);
-            cardRepository.save(entity);
-            throw new RuntimeException(e);
-        }
-        log.debug("Bard 실패");
-        entity.setLlmStatus(LLMStatusENUM.CANCELED);
-        cardRepository.save(entity);
-        running = false;
-        return false;
-    }
-
-    @Override
-    public void completeWating(CardDTO dto, long key) {
-        while (true) {
-            try {
-                Thread.sleep(3000); //3초 대기
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            if (dto.getLlmStatus() == LLMStatusENUM.COMPLETED || dto.getLlmStatus() == LLMStatusENUM.CANCELED) {
-                broadcastService.cardBroadcast(dto, key);
-                break;
-            }
-        }
-    }
-}
+//package ce.daegu.ac.kr.aStartrip.service;
+//
+//import ce.daegu.ac.kr.aStartrip.broadcast.BroadcastService;
+//import ce.daegu.ac.kr.aStartrip.dto.CardDTO;
+//import ce.daegu.ac.kr.aStartrip.entity.Card;
+//import ce.daegu.ac.kr.aStartrip.entity.LLMStatusENUM;
+//import ce.daegu.ac.kr.aStartrip.repository.CardRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Optional;
+//
+//@RequiredArgsConstructor
+//@Service
+//@Slf4j
+//@Component
+//public class LLMServiceBard implements LLMService {
+//    private final CardRepository cardRepository;
+//    private final BroadcastService broadcastService;
+//
+//    @Value("${python.path}")
+//    private String pythonPath;
+//
+//    public static boolean running = false;
+//
+//    @Override
+//    public boolean execute(Card entity) {
+//        while (running) {
+//            try {
+//                Thread.sleep(6000); //6초 대기
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        try {
+//            Thread.sleep(2000); //2초 대기 어뷰징 방지용
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        log.debug("현재 디렉터리:{}", System.getProperty("user.dir"));
+//
+//        try {
+//            running = true;
+//            Process process = new ProcessBuilder
+//                    (pythonPath, "./src/main/bard.py",
+//                            String.valueOf(entity.getId())).start();
+//            // $ python ./src/main/bard.py 카드ID
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                log.debug("Bard python: {}", line);
+//                if (line.trim().equals("true")) {
+//                    log.debug("Bard 성공");
+//                    running = false;
+//                    return true;
+//                }
+//            }
+//        } catch (IOException e) {
+//            entity.setLlmStatus(LLMStatusENUM.CANCELED);
+//            cardRepository.save(entity);
+//            throw new RuntimeException(e);
+//        }
+//        log.debug("Bard 실패");
+//        entity.setLlmStatus(LLMStatusENUM.CANCELED);
+//        cardRepository.save(entity);
+//        running = false;
+//        return false;
+//    }
+//
+//    @Override
+//    public void completeWating(CardDTO dto, long key) {
+//        while (true) {
+//            try {
+//                Thread.sleep(3000); //3초 대기
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            if (dto.getLlmStatus() == LLMStatusENUM.COMPLETED || dto.getLlmStatus() == LLMStatusENUM.CANCELED) {
+//                broadcastService.cardBroadcast(dto, key);
+//                break;
+//            }
+//        }
+//    }
+//}
---
+++ src/main/java/ce/daegu/ac/kr/aStartrip/service/LLMServicePPLX.java
@@ -0,0 +1,223 @@
+package ce.daegu.ac.kr.aStartrip.service;
+
+import ce.daegu.ac.kr.aStartrip.broadcast.BroadcastService;
+import ce.daegu.ac.kr.aStartrip.dto.CardDTO;
+import ce.daegu.ac.kr.aStartrip.entity.Card;
+import ce.daegu.ac.kr.aStartrip.entity.CardTypeENUM;
+import ce.daegu.ac.kr.aStartrip.entity.LLMStatusENUM;
+import ce.daegu.ac.kr.aStartrip.repository.CardRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+@Component
+public class LLMServicePPLX implements LLMService {
+    private final CardRepository cardRepository;
+    private final BroadcastService broadcastService;
+
+    @Value("${pplx.api.token}")
+    private String pplxApiToken;
+
+    public static boolean running = false;
+
+    @Override
+    public boolean execute(Card entity) {
+        while (running) {
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            Thread.sleep(2000); //2초 대기 어뷰징 방지용
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //main 로직
+        running = true;
+        String request_i;
+        CardTypeENUM card_type = entity.getCardType();
+        switch (card_type) {
+            case R01:
+                request_i = """
+                        I need to study English. Please generate English paragraph on any topic. It can be long
+                        """;
+                entity.setLLMResponse0(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+                break;
+            case R02:
+//                        Pre-reading question
+                request_i = """
+                        Please create a pre-reading question for the paragraph below. Please provide answers in English and Korean.
+                        ________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse0(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+//            Backgrond-knowledge
+                request_i = """
+                        Please create a background-knowledge for the paragraph below. Please provide answers in English and Korean.
+                        ________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse1(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+//            Post-reading question
+                request_i = """
+                        Please create a post-reading question for the paragraph below. Please provide answers in English and Korean.
+                        ________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse2(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+            case W01:
+                request_i = """
+                        I'm studying English. I'll send you a paragraph, so please evaluate whether the paragraph I sent is appropriate and correct it. Please reply in Korean.
+                        _________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse0(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+                break;
+            case W02:
+                request_i = """
+                        I'm studying English. I'll send you a sentence, so please analyze the composition of the sentence. Please reply in Korean.
+                        __________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+                break;
+            case V01:
+                request_i = """
+                        I am studying English. I'll send you the words separated by , so please create an English story using the words.
+                        _________
+                        words: 
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse0(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+                break;
+            case V02:
+//                       동의어
+                request_i = """
+                        I am studying English. I'll send you the word, so please find the relevant synonyms word. Find the word for english. and explain in Korean. 
+                        단어를 줄테니 relevant synonyms 찾아주세요. 아래는 단어 입니다.
+                        ________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse0(requestToAPI(request_i));
+//            반의어
+                request_i = """
+                        I am studying English. I'll send you the sentence, so please find the related antonym word. Find the word for english. and explain in Korean.
+                        단어를 줄테니 antonym 찾아주세요. 아래는 단어 입니다.
+                        ________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse1(requestToAPI(request_i));
+//            word-family
+                request_i = """
+                        I am studying English. I'll send you the sentence, so please find the word-family. Find the word for english. and explain in Korean.
+                        단어를 줄테니 Word-family 찾아주세요. 아래는 단어 입니다.
+                        ________
+                        
+                        """ + entity.getUserInput0();
+                entity.setLLMResponse2(requestToAPI(request_i));
+                entity.setLlmStatus(LLMStatusENUM.COMPLETED);
+                break;
+            default:
+                entity.setLlmStatus(LLMStatusENUM.CANCELED);
+                running = false;
+                cardRepository.save(entity);
+                return false;
+        }
+
+        running = false;
+        cardRepository.save(entity);
+        return true;
+    }
+
+    @Override
+    public void completeWating(CardDTO dto, long key) {
+        while (true) {
+            try {
+                Thread.sleep(3000); //3초 대기
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (dto.getLlmStatus() == LLMStatusENUM.COMPLETED || dto.getLlmStatus() == LLMStatusENUM.CANCELED) {
+                broadcastService.cardBroadcast(dto, key);
+                break;
+            }
+        }
+    }
+
+    public String requestToAPI(String request_i) {
+        String token = "Bearer " + pplxApiToken;
+        String requestBody = buildRequestBody(request_i);
+        HttpResponse<String> response = null;
+
+        try {
+            response = Unirest.post("https://api.perplexity.ai/chat/completions").header("Authorization", token).header("Content-Type", "application/json").body(requestBody).asString();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+        return extractContent(response.getBody());
+    }
+
+    public String buildRequestBody(String request_i) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("model", "llama-3.1-sonar-small-128k-online");
+        body.put("messages", List.of(Map.of("role", "system", "content", "Be precise and concise."), Map.of("role", "user", "content", request_i)));
+        body.put("temperature", 0.2);
+        body.put("top_p", 0.9);
+        body.put("return_citations", true);
+        body.put("search_domain_filter", List.of("perplexity.ai"));
+        body.put("return_images", false);
+        body.put("return_related_questions", false);
+        body.put("search_recency_filter", "month");
+        body.put("top_k", 0);
+        body.put("stream", false);
+        body.put("presence_penalty", 0);
+        body.put("frequency_penalty", 1);
+
+        return new JSONObject(body).toString();
+    }
+
+    public static String extractContent(String jsonResponse) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(jsonResponse);
+
+            JsonNode contentNode = rootNode.path("choices")
+                    .path(0)
+                    .path("message")
+                    .path("content");
+
+            if (contentNode.isMissingNode()) {
+                return "Content not found in the response";
+            }
+
+            return contentNode.asText();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error parsing JSON response";
+        }
+    }
+}
