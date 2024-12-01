12d4ee15b5d13fe98b548fbf0ed45fb1eeaf0c5d
imdhson <hhammer1234@gmail.com>
2024-12-01 16:42:36 +0900

    ㅇㅗ타 수정

--- src/main/java/ce/daegu/ac/kr/aStartrip/service/LLMService.java
+++ src/main/java/ce/daegu/ac/kr/aStartrip/service/LLMService.java
@@ -8,7 +8,7 @@ public interface LLMService {
     public boolean execute(Card entity);
     //이 함수 호출시 비동기로 처리하는 것 권장
 
-    void completeWating(CardDTO dto, long key);
+    void completeWaiting(CardDTO dto, long key);
 
     String requestToAPI(String request_i) throws Exception;
 }
